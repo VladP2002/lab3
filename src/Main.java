9425bdfd08f2f0cba158babf5aaa3d5cd1c7b44b
Vlad <vlad.pasca17@gmail.com>
2022-10-18 13:42:36 +0300

    commit v2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product product1 = new Product();
+        product1.setProductsMadeInFirstYear(100);
+        product1.setProductsSoldInFirstYear(80);
+        product1.setProductsMadeInSecondYear(90);
+        product1.setProductsSoldInSecondYear(90);
+
+        Product product2 = new Product();
+        product2.setProductsMadeInFirstYear(150);
+        product2.setProductsSoldInFirstYear(80);
+        product2.setProductsMadeInSecondYear(190);
+        product2.setProductsSoldInSecondYear(120);
+
+        Product product3 = new Product();
+        product3.setProductsMadeInFirstYear(230);
+        product3.setProductsSoldInFirstYear(80);
+        product3.setProductsMadeInSecondYear(80);
+        product3.setProductsSoldInSecondYear(10);
+
+        double[] productList = {product1.calculateDemand(), product2.calculateDemand(), product3.calculateDemand()};
+        Arrays.sort(productList);
+        System.out.println("\nThe sorted array is: ");
+        for (double productDemand : productList) {
+            System.out.print(productDemand + " ");
+        }
     }
 }
---
+++ src/Product.java
@@ -0,0 +1,112 @@
+import static java.lang.Math.abs;
+import static java.lang.Math.floor;
+
+public class Product {
+    private int productsMadeInFirstYear;
+
+    private int productsSoldInFirstYear;
+
+    private int productsMadeInSecondYear;
+
+    private int productsSoldInSecondYear;
+
+
+    public int getProductsMadeInFirstYear() {
+        return productsMadeInFirstYear;
+    }
+
+    public void setProductsMadeInFirstYear(int productsMadeInFirstYear) {
+        this.productsMadeInFirstYear = productsMadeInFirstYear;
+    }
+
+    public int getProductsSoldInFirstYear() {
+        return productsSoldInFirstYear;
+    }
+
+    public void setProductsSoldInFirstYear(int productsSoldInFirstYear) {
+        productsSoldInFirstYear = productsSoldInFirstYear;
+    }
+
+    public int getProductsMadeInSecondYear() {
+        return productsMadeInSecondYear;
+    }
+
+    public void setProductsMadeInSecondYear(int productsMadeInSecondYear) {
+        this.productsMadeInSecondYear = productsMadeInSecondYear;
+    }
+
+    public int getProductsSoldInSecondYear() {
+        return productsSoldInSecondYear;
+    }
+
+    public void setProductsSoldInSecondYear(int productsSoldInSecondYear) {
+        this.productsSoldInSecondYear = productsSoldInSecondYear;
+    }
+
+    /**
+     * \calculates the demand in the 1st year
+     * converting the int values in double
+     *
+     * @return the demand in the 1st year
+     */
+    public double demandInFirstYear() {
+        double firstYearMade = this.productsMadeInFirstYear;
+        double firstYearSold = this.productsSoldInFirstYear;
+        return firstYearSold / firstYearMade;
+    }
+
+    /**
+     * \calculates the demand in the 2nd year
+     * converting the int values in double
+     *
+     * @return the demand in the 2nd year
+     */
+    public double demandInSecondYear() {
+        //calculates the demand in the 2nd year
+        //converting the int values in double
+        double secondYearMade = this.productsMadeInSecondYear;
+        double secondYearSold = this.productsSoldInSecondYear;
+        return secondYearSold / secondYearMade;
+    }
+
+    /**
+     * calculates the demand
+     */
+    public double calculateDemand() {
+        return this.demandInFirstYear() / this.demandInSecondYear();
+    }
+
+    /**
+     * compares the demands from both years
+     *
+     * @return a, if the demand is incresing
+     * b, if the demand is stagnating
+     * c, if the demand is decreasing
+     */
+    public char compareDemand() {
+        double comparison = this.calculateDemand();
+        if (comparison < 1) {
+            return 'a';
+        } else if (1 < comparison || comparison < 1.10) {
+            return 'b';
+        } else {
+            return 'c';
+        }
+    }
+
+    /**
+     * calculates the optimal number of products to be made in the next year based on demand grow
+     * uses the method compareDemand() to decide which formula should be used
+     */
+    public int optimalProduction() {
+        int nextYearProducts = 0;
+        if (this.compareDemand() == 'a') {
+            nextYearProducts = (int) (floor(this.productsSoldInSecondYear * 1.50) + 1);
+        } else if (this.compareDemand() == 'b') {
+            nextYearProducts = (int) (floor(this.productsSoldInSecondYear * 1.10) + 1);
+        } else {
+            nextYearProducts = (int) (floor(this.productsSoldInSecondYear * 0.9) + 1);
+        }
+        return nextYearProducts;
+    }
+}
